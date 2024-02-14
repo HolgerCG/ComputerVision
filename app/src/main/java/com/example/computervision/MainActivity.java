c73b7597c7df21974a5a5d375374690ee5ff8fb6
LisLeoMeraC <84941789+LisLeoMeraC@users.noreply.github.com>
2024-02-14 10:24:38 -0500

    Puesta de lectura de datos de imagenes y validacion

--- app/src/main/java/com/example/computervision/MainActivity.java
+++ app/src/main/java/com/example/computervision/MainActivity.java
@@ -15,6 +15,9 @@ import android.view.View;
 import android.Manifest;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.computervision.ml.Modelflowers;
+import com.example.computervision.ml.Modelrostro;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.mlkit.vision.common.InputImage;
@@ -30,7 +33,14 @@ import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class MainActivity
@@ -69,6 +79,8 @@ public class MainActivity
             }
         }
     }
+
+
     public void abrirGaleria(View view) {
         // Abre a galeria de imagenes
         Intent i = new Intent(Intent.ACTION_PICK,
@@ -97,7 +109,7 @@ public class MainActivity
     public void onSuccess(Text text) {
         List<Text.TextBlock> blocks = text.getTextBlocks();
         String resultados="";
-        if (blocks.size() == 0) {
+        if (blocks.size() == 0  ) {
             resultados = "No hay Texto";
         }else{
             for (int i = 0; i < blocks.size(); i++) {
@@ -167,5 +179,67 @@ public class MainActivity
                 .addOnFailureListener(this);
     }
 
+    public void PersonalizedModel(View v){
+        try {
+            String[] etiquetas = {"Margarita", "Dientes de León", "Rosas", "Girasoles","Tulipanes"};
+
+            Modelflowers model = Modelflowers.newInstance(getApplicationContext());
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            inputFeature0.loadBuffer(convertirImagenATensorBuffer(mSelectedImage));
+            Modelflowers.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+            txtResults.setText(obtenerEtiquetayProbabilidad(etiquetas, outputFeature0.getFloatArray()));
+            model.close();
+        } catch (Exception e) {
+            txtResults.setText(e.getMessage());
+        }
+    }
+
+    public void PersonalizedModelRostro(View v){
+        try {
+            String[] etiquetas = {"Fernando", "Solange", "Domenica", "Leandro","Holger"};
+
+            Modelrostro model = Modelrostro.newInstance(getApplicationContext());
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            inputFeature0.loadBuffer(convertirImagenATensorBuffer(mSelectedImage));
+            Modelrostro.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+            txtResults.setText(obtenerEtiquetayProbabilidad(etiquetas, outputFeature0.getFloatArray()));
+            model.close();
+        } catch (Exception e) {
+            txtResults.setText(e.getMessage());
+        }
+    }
+
+    public ByteBuffer convertirImagenATensorBuffer(Bitmap mSelectedImage){
+        Bitmap imagen = Bitmap.createScaledBitmap(mSelectedImage, 224, 224, true);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * 224 * 224 * 3);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        int[] intValues = new int[224 * 224];
+        imagen.getPixels(intValues, 0, imagen.getWidth(), 0, 0, imagen.getWidth(), imagen.getHeight());
+        int pixel = 0;
+        for(int i = 0; i < imagen.getHeight(); i ++){
+            for(int j = 0; j < imagen.getWidth(); j++){
+                int val = intValues[pixel++]; // RGB
+                byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+            }
+        }
+        return byteBuffer;
+    }
+
+    public String obtenerEtiquetayProbabilidad(String[] etiquetas, float[] probabilidades){
+        float valorMayor=Float.MIN_VALUE;
+        int pos=-1;
+        for (int i = 0; i < probabilidades.length; i++) {
+            if (probabilidades[i] > valorMayor) {
+                valorMayor = probabilidades[i];
+                pos = i;
+            }
+        }
+        return "Predicción: " + etiquetas[pos] + ", Probabilidad: " +
+                (new DecimalFormat("0.00").format(probabilidades[pos] * 100)) + "%";
+    }
 
 }
